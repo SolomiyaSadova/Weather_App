1c1751d37eb40bdaa70851971cd0d1ff8c66084a
SolomiyaSadova <ssolimia@gmail.com>
2020-11-22 10:47:46 +0200

    main page

--- lib/main.dart
+++ lib/main.dart
@@ -49,8 +49,7 @@ class SearchPage extends StatefulWidget {
   _SearchPage createState() => _SearchPage();
 }
 
-class _SearchPage extends  State<SearchPage> {
-
+class _SearchPage extends State<SearchPage> {
   var cityController = TextEditingController();
 
   final _controller = TextEditingController();
@@ -67,103 +66,69 @@ class _SearchPage extends  State<SearchPage> {
   Widget build(BuildContext context) {
     final weatherBloc = BlocProvider.of<WeatherBloc>(context);
 
-
-
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-
-        BlocBuilder<WeatherBloc, WeatherState>(
-          builder: (context, state) {
-            if (state is WeatherIsNotSearched)
-              return Container(
-                padding: EdgeInsets.only(
-                  left: 32,
-                  right: 32,
-                ),
-                child: Column(
-                  children: <Widget>[
-                    Text(
-                      "Search Weather",
-                      style: TextStyle(
-                          fontSize: 40,
-                          fontWeight: FontWeight.w500,
-                          color: Colors.white70),
-                    ),
-                    SizedBox(
-                      height: 24,
-                    ),
-                    TextField(
-                      controller: _controller,
-                      readOnly: true,
-                      onTap: () async {
-                        // generate a new token here
-                        final sessionToken = Uuid().v4();
-                        final Suggestion result = await showSearch(
-                          context: context,
-                          delegate: AddressSearch(sessionToken),
-                        );
-                        // This will change the text displayed in the TextField
-                        if (result != null) {
-                          final placeDetails = await PlaceApiProvider(sessionToken)
-                              .getPlaceDetailFromId(result.placeId);
-                          setState(() {
-                            _controller.text = result.description;
-                            _city = placeDetails.city;
-                          });
-                        }
-                      },
-                      decoration: InputDecoration(
-                        icon: Container(
-                          width: 10,
-                          height: 10,
-                          child: Icon(
-                            Icons.home,
-                            color: Colors.white,
-
-                          ),
-                        ),
-                        hintText: "Enter your shipping address",
-                        hintStyle: TextStyle(color: Colors.white),
-                        border: InputBorder.none,
-                        contentPadding: EdgeInsets.only(left: 8.0, top: 16.0),
-                      ),
-                    ),
-                    SizedBox(height: 20.0),
-                    Container(
-                      width: double.infinity,
-                      height: 50,
-                      child: FlatButton(
-                        shape: new RoundedRectangleBorder(
-                            borderRadius:
-                            BorderRadius.all(Radius.circular(10))),
-                        onPressed: () {
+    return
+      DecoratedBox(
+        decoration: BoxDecoration(
+          image: DecorationImage(image: AssetImage("assets/fon.jpg"), fit: BoxFit.cover),
+        ),
+      child:
+      Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          BlocBuilder<WeatherBloc, WeatherState>(
+            builder: (context, state) {
+              if (state is WeatherIsNotSearched)
+                return Container(
+                  padding: EdgeInsets.fromLTRB(10, 22, 10, 2),
+                  child: TextField(
+                    controller: _controller,
+                    readOnly: true,
+                    onTap: () async {
+                      // generate a new token here
+                      final sessionToken = Uuid().v4();
+                      final Suggestion result = await showSearch(
+                        context: context,
+                        delegate: AddressSearch(sessionToken),
+                      );
+                      // This will change the text displayed in the TextField
+                      if (result != null) {
+                        final placeDetails =
+                        await PlaceApiProvider(sessionToken)
+                            .getPlaceDetailFromId(result.placeId);
+                        setState(() {
+                          _controller.text = result.description;
+                          _city = placeDetails.city;
                           weatherBloc.add(FetchWeather(_city));
-                        },
-                        color: Colors.lightBlue,
-                        child: Text(
-                          "Search",
-                          style: TextStyle(color: Colors.white70, fontSize: 16),
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-              );
-            else if (state is WeatherIsLoading)
-              return Center(child: CircularProgressIndicator());
-            else if (state is WeatherIsLoaded)
-              return ShowForecast(state.getForecast, _city);
-            else
-              return Text(
-                "Error",
-                style: TextStyle(color: Colors.white),
-              );
-          },
-        )
-      ],
-    );
+                        });
+                      }
+                    },
+                    style: TextStyle(fontSize: 22.0, color: Colors.white),
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      hintText: "Enter your address",
+                      hintStyle: TextStyle(
+                          color: Colors.black, fontStyle: FontStyle.italic),
+                      // contentPadding: EdgeInsets.only(left: 0.0, top: 0.0),
+                    ),
+                  ),
+                );
+              else if (state is WeatherIsLoading)
+                return Center(child: CircularProgressIndicator());
+              else if (state is WeatherIsLoaded)
+                return ShowForecast(state.getForecast, _city);
+              else
+                return Text(
+                  "Error",
+                  style: TextStyle(color: Colors.white),
+                );
+            },
+          )
+        ],
+      )
+    )
+        ;
   }
 }
 
@@ -192,7 +157,6 @@ class ShowForecast extends StatelessWidget {
         new SizedBox(
           height: 30,
         ),
-
       ],
     ));
 
@@ -245,11 +209,9 @@ class ShowForecast extends StatelessWidget {
             // Text(weather.hour,style: TextStyle(color: Colors.white70, fontSize: 14),),
             // Text(weather.getTemp.round().toString()+"C",style: TextStyle(color: Colors.white70, fontSize: 20),),
 
-            Row(
-                children: <Widget>[
-                  for (var item in forecast) getTextWidgets(item),
-
-                ]
+            Row(children: <Widget>[
+              for (var item in forecast) getTextWidgets(item),
+            ]
               //  getTextWidgets(),
             ),
 
